b6cb91b542b8559f134fb69b4a490093ab4e1c5a
Pavel Prichodko <prichodko.p@gmail.com>
2018-11-28 12:23:33 -0800

    feat(eslint-config-react): add React quality and stylistic rules

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -2,5 +2,124 @@
 
 module.exports = {
   extends: ['@strv/eslint-config-base'],
-  rules: {},
+  root: true,
+
+  parser: 'babel-eslint',
+
+  plugins: ['react', 'jsx-a11y'],
+
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  rules: {
+    // https://github.com/yannickcr/eslint-plugin-react#list-of-supported-rules
+    'react/boolean-prop-naming': [
+      'warn',
+      {
+        rule: '^(is|has)[A-Z]([A-Za-z0-9]?)+',
+        message:
+          'Boolean prop: ({{ propName }}) should start with is/has - pattern: ({{ pattern }})',
+      },
+    ],
+    'react/default-props-match-prop-types': 'warn',
+    'react/forbid-dom-props': [
+      'warn',
+      {
+        forbid: ['style'],
+      },
+    ],
+    'react/forbid-foreign-prop-types': ['error', { allowInPropTypes: true }],
+    'react/no-access-state-in-setstate': 'error',
+    'react/no-array-index-key': 'warn',
+    'react/no-danger-with-children': 'error',
+    'react/no-deprecated': 'warn',
+    'react/no-did-mount-set-state': ['error'],
+    'react/no-did-update-set-state': ['error'],
+    'react/no-direct-mutation-state': 'error',
+    'react/no-find-dom-node': 'error',
+    'react/no-is-mounted': 'error',
+    'react/no-multi-comp': [
+      'warn',
+      {
+        ignoreStateless: true,
+      },
+    ],
+    'react/no-redundant-should-component-update': 'warn',
+    'react/no-render-return-value': 'error',
+    'react/no-typos': 'error',
+    'react/no-string-refs': 'error',
+    'react/no-this-in-sfc': 'error',
+    'react/no-unescaped-entities': ['warn'],
+    'react/no-unknown-property': ['error'],
+    'react/no-unsafe': 'warn',
+    'react/no-unused-prop-types': ['warn'],
+    'react/no-unused-state': 'warn',
+    'react/no-will-update-set-state': ['warn'],
+    'react/prop-types': ['warn'],
+    'react/react-in-jsx-scope': 'error',
+    'react/require-default-props': ['warn', { forbidDefaultForRequired: true }],
+    'react/require-render-return': 'error',
+    'react/sort-comp': ['warn'],
+    'react/style-prop-object': 'error',
+    'react/void-dom-elements-no-children': 'error',
+
+    // https://github.com/yannickcr/eslint-plugin-react#jsx-specific-rules
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'react/jsx-handler-names': ['warn'],
+    'react/jsx-key': 'warn',
+    'react/jsx-no-bind': ['warn', { allowArrowFunctions: true }],
+    'react/jsx-no-comment-textnodes': 'warn',
+    'react/jsx-no-duplicate-props': ['warn', { ignoreCase: true }],
+    'react/jsx-no-target-blank': ['warn'],
+    'react/jsx-no-undef': 'error',
+    'react/jsx-curly-brace-presence': ['warn', 'never'],
+    'react/jsx-fragments': 'warn',
+    'react/jsx-pascal-case': [
+      'error',
+      {
+        allowAllCaps: true,
+        ignore: [],
+      },
+    ],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y#supported-rules
+    'jsx-a11y/accessible-emoji': 'warn',
+    'jsx-a11y/alt-text': 'warn',
+    'jsx-a11y/anchor-has-content': 'warn',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        aspects: ['noHref', 'invalidHref'],
+      },
+    ],
+    'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-role': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/heading-has-content': 'warn',
+    'jsx-a11y/iframe-has-title': 'warn',
+    'jsx-a11y/img-redundant-alt': 'warn',
+    'jsx-a11y/no-access-key': 'warn',
+    'jsx-a11y/no-distracting-elements': 'warn',
+    'jsx-a11y/no-redundant-roles': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'jsx-a11y/scope': 'warn',
+  },
 }
---
+++ packages/eslint-config-react/style.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = {
+  rules: {
+    'react/jsx-closing-bracket-location': 'warn',
+    'react/jsx-closing-tag-location': 'warn',
+    'react/jsx-curly-spacing': ['warn', { when: 'never', children: true }],
+    'react/jsx-equals-spacing': ['warn'],
+    'react/jsx-indent': ['warn'],
+    'react/jsx-indent-props': ['warn'],
+    'react/jsx-max-props-per-line': ['warn', { maximum: 2 }],
+    'react/jsx-one-expression-per-line': 'warn',
+    'react/jsx-props-no-multi-spaces': 'warn',
+    'react/jsx-tag-spacing': 'warn',
+    'react/jsx-wrap-multilines': 'warn',
+  },
+}
